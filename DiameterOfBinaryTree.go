ebd6dde10b7fb335339a392540bb1ee213167b00
Alec Lang <lostlang@icloud.com>
2022-08-06 23:59:14 +0300

    543. Diameter of Binary Tree

--- DiameterOfBinaryTree.go
+++ DiameterOfBinaryTree.go
@@ -1,5 +1,22 @@
 package leetcode
 
 func diameterOfBinaryTree(root *TreeNode) int {
-	return maxDepth(root.Left) + maxDepth(root.Right)
+	if root == nil {
+		return 0
+	}
+
+	return max3(maxDepth(root.Left)+maxDepth(root.Right), diameterOfBinaryTree(root.Left), diameterOfBinaryTree(root.Right))
+}
+
+func max3(a, b, c int) int {
+	if a > b {
+		if a > c {
+			return a
+		}
+		return c
+	}
+	if b > c {
+		return b
+	}
+	return c
 }
--- DiameterOfBinaryTree_test.go
+++ DiameterOfBinaryTree_test.go
@@ -13,7 +13,7 @@ type diameterOfBinaryTreeTestPair struct {
 var diameterOfBinaryTreeTestCases = []diameterOfBinaryTreeTestPair{
 	{generateTreeNode([]interface{}{1, 2, 3, 4, 5}), 3},
 	{generateTreeNode([]interface{}{1, 2}), 1},
-	{generateTreeNode([]interface{}{4, -7, -3, nil, nil, -9, -3, 9, -7, -4, nil, 6, nil, -6, -6, nil, nil, 0, 6, 5, nil, 9, nil, nil, -1, -4, nil, nil, nil, -2}), 7},
+	{generateTreeNode([]interface{}{4, -7, -3, nil, nil, -9, -3, 9, -7, -4, nil, 6, nil, -6, -6, nil, nil, 0, 6, 5, nil, 9, nil, nil, -1, -4, nil, nil, nil, -2}), 8},
 }
 
 func TestEvalDiameterOfBinaryTree(t *testing.T) {
